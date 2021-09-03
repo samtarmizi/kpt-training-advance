5e05065cdcaf685e337e27e9819e350ead4d2e62
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2021-09-03 10:56:49 +0800

    day3: show attachment

--- resources/views/schedules/show.blade.php
+++ resources/views/schedules/show.blade.php
@@ -8,14 +8,26 @@
                 <div class="card-header">{{ __('Schedule Show') }}</div>
 
                 <div class="card-body">
+                    <div class="form-group">
+                        <label>Title</label>
+                        <input type="text" name="title" class="form-control" value="{{ $schedule->title }}" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label>Description</label>
+                        <textarea name="description" class="form-control" readonly>{{ $schedule->description }}</textarea>
+                    </div>
+                    @if($schedule->attachment)
                         <div class="form-group">
-                            <label>Title</label>
-                            <input type="text" name="title" class="form-control" value="{{ $schedule->title }}" readonly>
+                            <label>Attachment (if any)</label>
+                            <a
+                                target="_blank"
+                                href="{{ asset('storage/'.$schedule->attachment) }}"
+                                class="btn btn-warning">
+
+                                Open this attachment: {{ $schedule->attachment }}
+                            </a>
                         </div>
-                        <div class="form-group">
-                            <label>Description</label>
-                            <textarea name="description" class="form-control" readonly>{{ $schedule->description }}</textarea>
-                        </div>
+                    @endif
                 </div>
             </div>
         </div>
